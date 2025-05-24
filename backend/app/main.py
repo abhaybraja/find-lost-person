7f15e5463858c3ee28618f43a8c247a1a27c8259
Abhay Braja <72004597+abhaybraja@users.noreply.github.com>
2025-05-24 13:50:01 +0530

    models installed

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.api.routes import face_search
 from app.db.db import Base, engine
-app = FastAPI(title="Face Search API")
+app = FastAPI(title="Face Search API @abhaybraja")
 
 Base.metadata.create_all(bind=engine)
 
